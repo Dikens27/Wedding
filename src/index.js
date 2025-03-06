4a825aa0270148c1dbf5d2381cbf3c96d6d2867b
Dikens27 <kar.karovich321@gmail.com>
2025-03-06 13:11:07 +0200

    animations

---
+++ src/index.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.hero, .guests-block, .place-block', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px',
+  reset: false,
+});
+
+ScrollReveal().reveal('.date-block, .timing-block, .wish-block', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px',
+  reset: false,
+});
